1c65f09b93915f18caf7c53551e370b6006ee8e3
fjking171 <16769161+fjking171@users.noreply.github.com>
2026-06-01 12:52:32 +1000

    fixed rotation logic, added object groups for updating and drawing

--- main.py
+++ main.py
@@ -11,6 +11,11 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0.0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+
     player = Player((SCREEN_WIDTH / 2),(SCREEN_HEIGHT /2))
 
     while True:
@@ -20,7 +25,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        updatable.update(dt)
+        for d in drawable:
+            d.draw(screen)
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
